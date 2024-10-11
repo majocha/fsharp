34d2bca97311c4131344dbe987c696648d72539a
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2024-10-11 14:25:07 +0200

    wip

--- tests/FSharp.Core.UnitTests/FSharp.Core/Microsoft.FSharp.Control/MailboxProcessorType.fs
+++ tests/FSharp.Core.UnitTests/FSharp.Core/Microsoft.FSharp.Control/MailboxProcessorType.fs
@@ -24,7 +24,7 @@ type StartImmediateThreadInfo =
 type StartImmediateMessage =
     | GetThreadInfo of AsyncReplyChannel<StartImmediateThreadInfo>
 
-// [<Collection(nameof FSharp.Test.DoNotRunInParallel)>]
+[<Collection(nameof FSharp.Test.DoNotRunInParallel)>]
 type MailboxProcessorType() =
 
     let getSimpleMailbox() =
